e40bcf9140b68cfdee6a87d8dff5a2b7dd7f322e
Lincoln16yyy <kakuoriginal3@outlook.com>
2026-05-22 18:22:02 -0300

    first commit

---
+++ src/test/java/com/example/cadastrosdeninjas/CadastrosDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadastrosdeninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosDeNinjasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
